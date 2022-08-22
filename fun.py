089b9cceef31cf4d8d9709e41d953e254f1df11a
Brayan Camacho <brayanchistosoje8@gmail.com>
2022-08-21 21:22:23 -0700

    Meme and Fight commands moved to slash commands.

--- fun.py
+++ fun.py
@@ -1,63 +1,54 @@
 import discord
+import typing
 import asyncio
 from APIs import get_meme, get_wholesomememe, get_dankmeme
 from discord.ext import commands
+from discord import app_commands
 
 
-# Fun module, for fun commands
+# Fun Commands Module
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     # The meme command
-    @commands.hybrid_command(name='meme',
-                             description='Get a meme, options are: wholesome and dank. For random leave empty',
-                             aliases=['memes'],
-                             help='Wholesome, Dank, or Random')
-    async def memes(self, ctx, memetype: str = ''):
-        if memetype == '' or memetype.lower() == 'random':
-            embed = discord.Embed(title='Here is your random meme!')
-            embed.set_image(url=get_meme())
-            await ctx.channel.send(embed=embed)
+    @app_commands.command(name='meme',
+                          description='Get a meme, options are: wholesome and dank. For random leave empty',
+                          )
+    @app_commands.describe(memetype='Kind of Meme')
+    async def memes(self, interaction: discord.Interaction,
+                    memetype: typing.Optional[typing.Literal['random', 'wholesome', 'dank']] = None):
+        embed = discord.Embed(color=discord.Colour.dark_purple())
+        if memetype is None or memetype.lower() == 'random':
+            await interaction.response.send_message('Here is your random meme!',
+                                                    embed=embed.set_image(url=get_meme()))
         elif memetype.lower() == 'wholesome':
-            embed = discord.Embed(title='Here is your wholesome meme!')
-            embed.set_image(url=get_wholesomememe())
-            await ctx.channel.send(embed=embed)
+            await interaction.response.send_message('Here is your wholesome meme!',
+                                                    embed=embed.set_image(url=get_wholesomememe()))
         elif memetype.lower() == 'dank':
-            embed = discord.Embed(title='Here is your dank meme!')
-            embed.set_image(url=get_dankmeme())
-            await ctx.channel.send(embed=embed)
-        else:
-            await ctx.channel.send('That is not a supported type of meme!')
-
-    # The "Help" command.
-    @commands.command(name='thehelp', description='The TRUE help command.', hidden=True)
-    async def nohelp(self, ctx):
-        await ctx.channel.send('No help for you sucka!!!1!')
+            await interaction.response.send_message('Here is your dank meme!',
+                                                    embed=embed.set_image(url=get_dankmeme()))
 
     # Fight command, in the future it will be much different.
-    @commands.command(name='fight')
-    async def fight(self, ctx, who: discord.User = None):
-        # Takes the author of the message, or whoever sends the command.
-        author = ctx.author
-        if ctx.author.bot:
-            return
+    @app_commands.command(name='fight', description='Challenge someone\'s sorry self.')
+    async def fight(self, interaction: discord.Interaction, who: discord.User):
+        author = interaction.user
         if who:
-            await ctx.send('IS NOT SMART ==> {}'.format(who.mention))
+            await interaction.response.send_message('IS NOT SMART ==> {}'.format(who.mention))
             await asyncio.sleep(1)
-            await ctx.send('Would you like to \'Fight back\' or \'Give up\'?')
+            await interaction.edit_original_response(content='IS NOT SMART ==> {} \n'
+                                                             'Would you like to \'Fight back\' or \'Give up\'?'
+                                                     .format(who.mention))
 
             def check(message):
-                return message.author == who and message.channel == ctx.channel and \
+                return message.author == who and message.channel == interaction.channel and \
                        message.content.lower() in ['fight back', 'give up']
 
             msg = await self.bot.wait_for("message", check=check, timeout=120)
             if msg.content.lower() == 'fight back':
-                await ctx.send('Is stinky ==> {}'.format(author.mention))
+                await interaction.followup.send('Is stinky ==> {}'.format(author.mention))
             else:
-                await ctx.send('{} gave up!'.format(who.mention))
-        else:
-            await ctx.send('Mention someone to fight in the command.')
+                await interaction.followup.send('{} gave up!'.format(who.mention))
 
 
 # Add the cog to the bot
