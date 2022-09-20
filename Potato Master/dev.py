89798d1241c8b0c85bb5d108d12f832954251d3d
Brayan Camacho <brayanchistosoje8@gmail.com>
2022-09-19 17:42:03 -0700

    Will make use of this now.

--- Potato Master/dev.py
+++ Potato Master/dev.py
@@ -1,52 +1,23 @@
-import discord
 import typing
+import discord
+from discord import app_commands
 from discord.ext import commands
+from hg import MongoDB
+
+"""
+
+    This module is solely for the purpose of developing and testing new commands that could potentially mess up the
+    bot or could lead to something going wrong with the core functionality of the bot.
+    
+    Or just if I want to leave some existing command alone while I make a new version of it here.
+
+"""
 
 
-# Developer Commands Module
-class Dev(commands.Cog):
+# Hunger Games MESSAGES module
+class Dev(commands.GroupCog, name='dev', description='Commands in development.'):
     def __init__(self, bot):
         self.bot = bot
-
-    # SYNC COMMAND
-    @commands.command(name='sync_dev')
-    @commands.guild_only()
-    async def sync(self, ctx: commands.Context,
-                   guilds: commands.Greedy[discord.Object],
-                   spec: typing.Optional[typing.Literal["~", "*", "^"]] = None) -> None:
-        if not guilds:
-            if spec == "~":
-                synced = await ctx.bot.tree.sync(guild=ctx.guild)
-            elif spec == "*":
-                ctx.bot.tree.copy_global_to(guild=ctx.guild)
-                synced = await ctx.bot.tree.sync(guild=ctx.guild)
-            elif spec == "^":
-                ctx.bot.tree.clear_commands(guild=ctx.guild)
-                await ctx.bot.tree.sync(guild=ctx.guild)
-                synced = []
-            else:
-                synced = await ctx.bot.tree.sync()
-
-            await ctx.send(
-                f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
-            )
-            return
-
-        ret = 0
-        for guild in guilds:
-            try:
-                await ctx.bot.tree.sync(guild=guild)
-            except discord.HTTPException:
-                pass
-            else:
-                ret += 1
-
-            await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
-
-    # The test command, does nothing
-    @commands.hybrid_command(name='test', description="Testing")
-    async def test(self, ctx: commands.Context):
-        await ctx.send('Test Complete!')
 
 
 # Add the cog to the bot
