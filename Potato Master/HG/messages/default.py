99cbeaea90205632f7992a02f69f978f58581152
Brayan Camacho <brayanchistosoje8@gmail.com>
2022-10-24 16:34:47 -0700

    CHANGES!

---
+++ Potato Master/HG/messages/default.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+default_alive = np.array([
+    "**Managed to live!**", "**Lived through a nuclear holocaust!**", "**Had a nice day!**",
+    "**Ran into a bunny, then ran for their life!**", "**Thought it was a good idea to try and eat the bot. They succeeded!**",
+    "**Tried to run away from the cops, and then got high.**"
+])
+
+
+default_dead = np.array([
+    "*Tripped and snapped their neck.*", "*Insulted the owner of the discord.*",
+    "*Died due to pun overdose.*", "*Death by Snu Snu.*", "*Got hit by a train, and then was finished off by a moose!*",
+    "*Had a lack of faith in J.C.*", "*Thought it was a good idea to try and eat the bot. They failed.*",
+    "*Couldn't take it anymore.*", "*Found some food, then found some food poisoning.*",
+    "*I've fallen, and I can't get up!*"
+])
+
+
+default_wounded = np.array([
+    "***Fell off a cliff but survived! Not without a few broken bones though...***",
+    "***Survived a monkey attack! Far from unscathed though...***"
+])
